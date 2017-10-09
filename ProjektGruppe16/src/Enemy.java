892b23338e4cb304d8f63ff2f9a685439317c1da
BangPoulsen <rasmus@rasmuspoulsen.dk>
2017-10-09 12:50:45 +0200

    Added Inventory

--- ProjektGruppe16/src/Enemy.java
+++ ProjektGruppe16/src/Enemy.java
@@ -1,17 +1,21 @@
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+
 public class Enemy {
     
     private int health;
     private String name;
     private int damage;
-    private int range;
-            
-    public void Attack(){
-        
-    }
-            
+    private int range;   
     
     public Enemy(){
+        
+    }
+    
+    public void Attack(){
         
     }
 }
