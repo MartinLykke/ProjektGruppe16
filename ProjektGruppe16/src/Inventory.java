bd1da2720a068915897ef275d394dfaa9634cb2a
BangPoulsen <rasmus@rasmuspoulsen.dk>
2017-10-09 13:23:46 +0200

    Added comments

--- ProjektGruppe16/src/Inventory.java
+++ ProjektGruppe16/src/Inventory.java
@@ -1,19 +1,23 @@
-
+/*
+Class for creating and managing the inventory
+*/
 import java.util.ArrayList;
 
 public class Inventory {
-    private ArrayList<Item> inventory;
-    private int capacity;
+    private ArrayList<Item> inventory; //List of the items in inventory
+    private int capacity; //Amount of items a player can carry
     
     public Inventory(){
         inventory = new ArrayList<>();
         capacity = 4;
     }
     
+    //Checks if inventory is full
     public boolean inventoryFull(){
         return this.inventory.size() < this.capacity;
     }
     
+    //Adds an item to the inventory
     public void add(Item item){
         this.inventory.add(item);
     }
