9ef7c267d1889e8ba87eab226ebbae8e7480b704
MartinLykke <32263396+MartinLykke@users.noreply.github.com>
2017-10-09 10:48:47 +0200

    Test 4

--- ProjektGruppe16/src/Enemy.java
+++ ProjektGruppe16/src/Enemy.java
@@ -4,6 +4,12 @@ public class Enemy {
     private int health;
     private String name;
     private int damage;
+    private int range;
+            
+    public void Attack(){
+        
+    }
+            
     
     public Enemy(){
         
