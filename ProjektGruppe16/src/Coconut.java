0facc0d7b5e6ce1de3767b22ed442414c92f95ea
BangPoulsen <rasmus@rasmuspoulsen.dk>
2017-10-12 12:31:09 +0200

    Added destroy method to Coconut

--- ProjektGruppe16/src/Coconut.java
+++ ProjektGruppe16/src/Coconut.java
@@ -1,9 +1,11 @@
 
 public class Coconut extends Food {
-    public Coconut (int restorevalue, int weight){
-        this.weight = weight;
-        this.restorevalue = restorevalue;
-        
+    public Coconut (){
+        this.name = "Coconut";
+        this.restorevalue = 10;
     }
     
+    public void destroy(){
+        this.destroy();
+    }
 }
--- ProjektGruppe16/src/Food.java
+++ ProjektGruppe16/src/Food.java
@@ -1,18 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author marti
- */
 public class Food extends Item{
     
-    private int restorevalue;
-    public Food(String name, int restorevalue){
-        this.name = name;
-        this.restorevalue = restorevalue;
-    }
+    public int restorevalue;
 }
