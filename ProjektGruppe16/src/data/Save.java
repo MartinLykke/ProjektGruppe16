2394c3707852459dd51916d4c24d0c4dde0f2836
BangPoulsen <rasmus@rasmuspoulsen.dk>
2017-12-16 18:45:19 +0100

    Comments

--- ProjektGruppe16/src/data/Save.java
+++ ProjektGruppe16/src/data/Save.java
@@ -21,6 +21,12 @@ public class Save {
     private Room loadedRoom;
     private Player loadedPlayer;
     
+    /**
+     * Takes a Player and Room object as input and then adds them to a 
+     * SaveFile object which is then written to a file using serialization
+     * @param player The Player object 
+     * @param currentRoom 
+     */
     public void save(Player player, Room currentRoom){
         save = new SaveFile(player, currentRoom);
         
@@ -35,6 +41,13 @@ public class Save {
         }
     }
     
+    /**
+     * Method for loading a SaveFile object from a file created by the 
+     * save() method and then loads it's Player and Room object into temporary
+     * variables for loading into the game.
+     * @param fileLocation The location of the file to load the objects from,
+     * including the filename of the file to load. E.g. "savefiles/save.save"
+     */
     public void load(String fileLocation){
         try {
             FileInputStream loadFile = new FileInputStream(fileLocation);
@@ -50,10 +63,18 @@ public class Save {
         loadedRoom = save.currentRoom;
     }
     
+    /**
+     * Method for getting a loaded Player object
+     * @return The Player object loaded from a file by the load() method
+     */
     public Player getLoadedPlayer(){
         return loadedPlayer;
     }
     
+    /**
+     * Method for getting a loaded Room object
+     * @return The Room object loaded from a file by the load() method
+     */
     public Room getloadedRoom(){
         return loadedRoom;
     }
